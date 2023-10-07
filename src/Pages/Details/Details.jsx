5e83531176de75ea3ac48e1a0911ad9bd23320f3
rhmunna143 <rhmunna19@gmail.com>
2023-10-07 17:23:48 +0600

    details dynamic route created

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,9 +1,35 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Details = () => {
+
+    const [data, setData] = useState([])
+    const params = useParams()
+
+    useEffect(() => {
+        fetch("../../../public/data.json")
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
+
+    const selected = data?.find(item => params.id == item.id);
+
+    const { image, title, description, details, price } = selected || {};
+
     return (
-        <div>
-            Details
+        <div className="max-w-6xl mx-auto mb-20">
+            <div className="img mt-20">
+                <img className="w-3/4 mx-auto" src={image} alt="details img" />
+            </div>
+
+            <div className="w-3/4 mx-auto mt-5">
+                <h2 className="text-4xl text-pink-600">{title}</h2>
+                <p className="text-slate-600 mt-2 text-lg">{price}</p>
+
+                <p className="mt-2">{description}</p>
+
+                <p className="mt-2">{details}</p>
+            </div>
         </div>
     );
 };
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -14,7 +14,7 @@ const Services = () => {
     }, [])
 
     return (
-        <div className="max-w-6xl mx-auto p-4 lg:p-0">
+        <div className="max-w-6xl mx-auto p-4 lg:p-0 mb-20">
             <h2 className="text-4xl text-center mb-10">Our <span className="text-pink-600">Services</span></h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">

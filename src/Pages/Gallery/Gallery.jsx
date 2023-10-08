c9c084d01575bbea0f36a6d1dd58ba9b2f707cc1
rhmunna143 <rhmunna19@gmail.com>
2023-10-08 20:26:06 +0600

    gallery page completed

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 
 
 const GalleryComp = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("/gallery.json")
+        .then(res => res.json())
+        .then(data => setData(data))
+    }, [])
+
     return (
-        <div>
-            Gallery
+        <div className="max-w-6xl mx-auto my-20 p-4 lg:p-0">
+            <h2 className="text-4xl text-pink-600 text-center my-10">Gallery of Our Events</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {
+                    data?.map(item=>
+                        <img className="h-[250px] w-[375px]" key={item?.id} src={item?.image} alt="gallery images" />
+                    )
+                }
+            </div>
         </div>
     );
 };

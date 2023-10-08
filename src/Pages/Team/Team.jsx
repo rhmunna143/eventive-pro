f1a774cb9da745d5ed4a52ef471c765fa7bfc856
rhmunna143 <rhmunna19@gmail.com>
2023-10-08 20:33:49 +0600

    teams page completed

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,9 +1,24 @@
-
+import { useEffect, useState } from "react";
 
 const TeamCmp = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("/teams.json")
+        .then(res => res.json())
+        .then(data => setData(data))
+    }, [])
+
     return (
-        <div>
-            Team
+        <div className="max-w-6xl mx-auto my-20 p-4 lg:p-0">
+            <h2 className="text-4xl text-pink-600 text-center my-10">Our Expert Teams</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {
+                    data?.map(item=>
+                        <img className="h-[250px] w-[375px]" key={item?.id} src={item?.image} alt="gallery images" />
+                    )
+                }
+            </div>
         </div>
     );
 };
